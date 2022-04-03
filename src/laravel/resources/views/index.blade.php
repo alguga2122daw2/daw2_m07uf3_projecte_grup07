65e72168821a0c606b15f7d6c99610a7e2b90d3e
dagaco <dagaco2122daw@protonmail.com>
2022-04-04 01:50:01 +0200

    Indice cambiado, texto centrado y añadidas algunas lineas

--- src/laravel/resources/views/index.blade.php
+++ src/laravel/resources/views/index.blade.php
@@ -2,13 +2,17 @@
 
 @section('content')
 
-<h1>TouristRent</h1>
+<h1 class="d-flex justify-content-center">TouristRent</h1>
 <div class="mt-5">
   @if(session()->get('success'))
     <div class="alert alert-success">
       {{ session()->get('success') }}  
     </div>
   @endif
+</div>
 <div>
-<h!>Projecte nº2</h1>
+  <h2 class="d-flex justify-content-center" >M07 UF3: Desenvolupament Web en Entorn Servidor</h2>
+  <h3 class="d-flex justify-content-center">Projecte nº2</h3>
+  <h4 class="d-flex justify-content-center">Laravel, MySQL, PHP</h4>
+</div>
 @endsection
